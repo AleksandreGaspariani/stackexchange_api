2c596d6207ee06c461e714adf86c59709b4b9ab8
AleksandreGaspariani <aleksandregasparyan@gmail.com>
2024-02-14 18:00:26 +0400

    add middleware for auth routes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
-use App\Http\Controllers\LoginController;
-use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,7 +19,11 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('register',[RegisterController::class, 'store']);
+Route::post('register',[AuthController::class, 'register']);
+Route::post('login',[AuthController::class, 'login']);
 
-Route::post('login',[LoginController::class, 'login']);
-Route::post('logout',[LoginController::class, 'logout']);
+Route::group(['middleware' => ['auth:sanctum']], function(){
+
+    Route::post('logout',[AuthController::class, 'logout']);
+
+});
