1b19afbba2e2f439f497f1dc755f63a752077e4d
AleksandreGaspariani <aleksandregasparyan@gmail.com>
2024-02-15 23:29:02 +0400

    add column to logs table

--- database/migrations/2024_02_15_103335_user_request_log.php
+++ database/migrations/2024_02_15_103335_user_request_log.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('user_request_log', function (Blueprint $table) {
             $table->id();
             $table->integer('user_id');
+            $table->json('request_body');
             $table->json('response_body');
             $table->json('headers');
             $table->timestamps();
