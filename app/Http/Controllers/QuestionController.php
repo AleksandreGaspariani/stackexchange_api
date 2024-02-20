cc0634266cc31ad142ac3c7b454d94c1f82d90fd
AleksandreGaspariani <aleksandregasparyan@gmail.com>
2024-02-20 19:45:43 +0400

    creating QuestionCollection resource for QuestionController

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\QuestionCollection;
 use App\Services\ApiService;
 use Exception as ExceptionAlias;
 use Illuminate\Http\JsonResponse;
@@ -20,26 +21,31 @@ class QuestionController extends Controller
     /**
      * @throws ExceptionAlias
      */
-    public function index(){
-        return $this->api
-            ->sendRequest('questions');
+    public function index(): QuestionCollection
+    {
+        return new QuestionCollection([
+           $this->api
+            ->sendRequest('questions')
+        ]);
     }
 
     /**
      * @throws ExceptionAlias
      */
-    public function show($id): JsonResponse
+    public function show($id): QuestionCollection
     {
         $res = $this->api
             ->sendRequest('questions/'.$id);
+
+        if (empty($res['items'])){ throw new \Exception('Question not found or it deleted'); }
 
         $userId = $res['items'][0]['owner']['user_id'];
 
         $userInfo = $this->api
             ->sendRequest('users/'.$userId);
 
-        return response()->json([
-            'Question Title' => $res['items'][0]['title'],
+        return new QuestionCollection([
+            'Question' => $res['items'][0]['title'],
             'Publisher' => $userInfo['items'][0]['display_name'],
         ]);
     }
